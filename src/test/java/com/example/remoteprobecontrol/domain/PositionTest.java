bb69c289bb553b51875dd932fbc45533b2b4711e
Siddhant Jain (THBS) <siddhant.jain@virginmediao2.co.uk>
2026-01-22 18:23:39 +0000

    [SJ] Added all possible backward and forward movements for every possible direction.

--- src/test/java/com/example/remoteprobecontrol/domain/PositionTest.java
+++ src/test/java/com/example/remoteprobecontrol/domain/PositionTest.java
@@ -16,6 +16,33 @@ public class PositionTest {
     }
 
     @Test
+    void moveForwardWhenFacingEastIncrementsX() {
+        Position start = new Position(0, 0);
+
+        Position result = start.moveForward(Direction.EAST);
+
+        assertEquals(new Position(1, 0), result);
+    }
+
+    @Test
+    void moveForwardWhenFacingSouthDecrementsY() {
+        Position start = new Position(0, 0);
+
+        Position result = start.moveForward(Direction.SOUTH);
+
+        assertEquals(new Position(0, -1), result);
+    }
+
+    @Test
+    void moveForwardWhenFacingWestIncrementsX() {
+        Position start = new Position(0, 0);
+
+        Position result = start.moveForward(Direction.WEST);
+
+        assertEquals(new Position(-1, 0), result);
+    }
+
+    @Test
     void moveBackwardWhenFacingNorthDecrementsY() {
         Position start = new Position(0, 0);
 
@@ -25,10 +52,28 @@ public class PositionTest {
     }
 
     @Test
-    void moveForwardWhenFacingEastIncrementsX() {
+    void moveBackwardWhenFacingSouthIncrementsY() {
         Position start = new Position(0, 0);
 
-        Position result = start.moveForward(Direction.EAST);
+        Position result = start.moveBackward(Direction.SOUTH);
+
+        assertEquals(new Position(0, 1), result);
+    }
+
+    @Test
+    void moveBackwardWhenFacingEastDecrementsX() {
+        Position start = new Position(0, 0);
+
+        Position result = start.moveBackward(Direction.EAST);
+
+        assertEquals(new Position(-1, 0), result);
+    }
+
+    @Test
+    void moveBackwardWhenFacingWestIncrementsX() {
+        Position start = new Position(0, 0);
+
+        Position result = start.moveBackward(Direction.WEST);
 
         assertEquals(new Position(1, 0), result);
     }
