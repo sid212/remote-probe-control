938dc9c222153d8ae777ae12661616eb07028b89
Siddhant Jain (THBS) <siddhant.jain@virginmediao2.co.uk>
2026-01-22 13:45:06 +0000

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/remoteprobecontrol/RemoteProbeControlApplication.java
@@ -0,0 +1,13 @@
+package com.example.remoteprobecontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteProbeControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemoteProbeControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/remoteprobecontrol/RemoteProbeControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.remoteprobecontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoteProbeControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
