61caa005279b28a1e34eea736b97d3ce18c302d6
Siddhant Jain (THBS) <siddhant.jain@virginmediao2.co.uk>
2026-01-22 17:12:14 +0000

    [SJ] Added failing tests for direction turning

---
+++ src/test/java/com/example/remoteprobecontrol/domain/DirectionTest.java
@@ -0,0 +1,16 @@
+package com.example.remoteprobecontrol.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DirectionTest {
+    @Test
+    void northTurnsLeftToWest() {
+        Direction direction = Direction.NORTH;
+
+        Direction result = direction.turnLeft();
+
+        assertEquals(Direction.WEST, result);
+    }
+}
