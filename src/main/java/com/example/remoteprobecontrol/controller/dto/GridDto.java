bb4cf84566b1d2b70291d5b17e83c93f88eacbd5
Siddhant Jain (THBS) <siddhant.jain@virginmediao2.co.uk>
2026-01-23 10:27:43 +0000

    [SJ] Implemented Obstracle optional functionality.

--- src/main/java/com/example/remoteprobecontrol/controller/dto/GridDto.java
+++ src/main/java/com/example/remoteprobecontrol/controller/dto/GridDto.java
@@ -1,13 +1,17 @@
 package com.example.remoteprobecontrol.controller.dto;
 
-import com.example.remoteprobecontrol.domain.Position;
-
-import java.util.List;
 import java.util.Set;
 
 public record GridDto (
     int width,
     int height,
-    Set<ObstracleDto> obstacles){}
+    Set<ObstracleDto> obstacles){
+
+    public GridDto {
+        if (obstacles == null) {
+            obstacles = Set.of(); // default empty set
+        }
+    }
+}
 
 
