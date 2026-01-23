729486e56f11da31578d20015ad3b21b0a7e399c
Siddhant Jain (THBS) <siddhant.jain@virginmediao2.co.uk>
2026-01-23 05:50:13 +0000

    [SJ] Support obstacles in grid validation

--- src/main/java/com/example/remoteprobecontrol/domain/Grid.java
+++ src/main/java/com/example/remoteprobecontrol/domain/Grid.java
@@ -1,18 +1,27 @@
 package com.example.remoteprobecontrol.domain;
 
+import java.util.Set;
+
 public class Grid {
     private final int width;
     private final int height;
+    private final Set<Position> obstacles;
 
     public Grid(int width, int height) {
+        this(width,height,Set.of());
+    }
+
+    public Grid(int width, int height, Set<Position> obstacles) {
         this.width = width;
         this.height = height;
+        this.obstacles = obstacles;
     }
 
     public boolean isValid(Position position) {
-        return position.getX() >= 0 &&
+        boolean insideBounds = position.getX() >= 0 &&
                 position.getX() < width &&
                 position.getY() >= 0 &&
                 position.getY() < height;
+        return insideBounds && !obstacles.contains(position);
     }
 }
