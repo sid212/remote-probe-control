5db26b43f9b0d3459f6bdc79bd12d7c757f88ae8
Siddhant Jain (THBS) <siddhant.jain@virginmediao2.co.uk>
2026-01-23 05:11:51 +0000

    [SJ] Added fail test for probe turning left and right direction.

--- src/test/java/com/example/remoteprobecontrol/domain/ProbeTest.java
+++ src/test/java/com/example/remoteprobecontrol/domain/ProbeTest.java
@@ -26,4 +26,24 @@ public class ProbeTest {
         assertEquals(Direction.NORTH, result.getDirection());
     }
 
+    @Test
+    void probeTurnsLeftWithoutChangingPosition(){
+        Probe probe = new Probe(new Position(1,1), Direction.NORTH);
+
+        Probe result = probe.turnLeft();
+
+        assertEquals(new Position(1,1), result.getPosition());
+        assertEquals(Direction.WEST, result.getDirection());
+    }
+
+    @Test
+    void probeTurnsRightWithoutChangingPosition(){
+        Probe probe = new Probe(new Position(1,1), Direction.NORTH);
+
+        Probe result = probe.turnRight();
+
+        assertEquals(new Position(1,1), result.getPosition());
+        assertEquals(Direction.EAST, result.getDirection());
+    }
+
 }
